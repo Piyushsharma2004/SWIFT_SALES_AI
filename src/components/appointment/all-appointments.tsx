a0a2aa113f5a26b4de3af91eee3acd1b9c888b7c
Piyush Sharma <111640586+Piyushsharma2004@users.noreply.github.com>
2024-07-19 07:43:29 +0530

    change appointment

--- src/components/appointment/all-appointments.tsx
+++ src/components/appointment/all-appointments.tsx
@@ -25,37 +25,7 @@ type Props = {
 
 const AllAppointments = ({ bookings }: Props) => {
   return (
-    <DataTable headers={APPOINTMENT_TABLE_HEADER}>
-      {bookings ? (
-        bookings.map((booking) => (
-          <TableRow key={booking.id}>
-            <TableCell>{booking.email}</TableCell>
-            <TableCell>
-              <div>
-                {getMonthName(booking.date.getMonth())} {booking.date.getDate()}{' '}
-                {booking.date.getFullYear()}
-              </div>
-              <div className="uppercase">{booking.slot}</div>
-            </TableCell>
-            <TableCell>
-              <div>
-                {getMonthName(booking.createdAt.getMonth())}{' '}
-                {booking.createdAt.getDate()} {booking.createdAt.getFullYear()}
-              </div>
-              <div>
-                {booking.createdAt.getHours()} {booking.createdAt.getMinutes()}{' '}
-                {booking.createdAt.getHours() > 12 ? 'PM' : 'AM'}
-              </div>
-            </TableCell>
-            <TableCell className="text-right">
-              {booking.Customer?.Domain?.name}
-            </TableCell>
-          </TableRow>
-        ))
-      ) : (
-        <CardDescription>No Appointments</CardDescription>
-      )}
-    </DataTable>
+    <h1>appointment</h1>
   )
 }
 
